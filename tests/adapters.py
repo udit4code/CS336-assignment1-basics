fea041c05c0badd60db86aaf3f85c6103200a6bf
udit4code <udit.sharma820@gmail.com>
2026-08-03 16:33:14 +0530

    feat: Added softmax impl

--- tests/adapters.py
+++ tests/adapters.py
@@ -592,7 +592,8 @@ def run_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, "
         Float[Tensor, "..."]: Tensor of with the same shape as `in_features` with the output of
         softmax normalizing the specified `dim`.
     """
-    raise NotImplementedError
+    from cs336_basics.TransformerImplementation.SoftmaxModule.Softmax import softmax
+    return softmax(in_features, dim)
 
 
 def run_cross_entropy(
